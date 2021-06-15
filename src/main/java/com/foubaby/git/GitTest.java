08bdc89910b20c5ac83835e75ca00f8cf2eb0660
子非鱼 <64509138+foubaby@users.noreply.github.com>
2021-06-15 11:02:28 +0800

    pull commit

--- src/main/java/com/foubaby/git/GitTest.java
+++ src/main/java/com/foubaby/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("fix-hot test...");
         System.out.println("push test...");
         System.out.println("push test2...");
+        System.out.println("pull test1...");
     }
 }
